fea7de71e3c3aa801046096b608fe5f5be940513
Hattie Samankul <hattie.samankul@hotmail.com>
2019-02-01 12:23:56 +0000

    make registeration form that take inputs and insert to database

--- JavaDatabase/src/ConnectionHandler.java
+++ JavaDatabase/src/ConnectionHandler.java
@@ -2,23 +2,43 @@ import java.sql.*;
 
 public class ConnectionHandler {
 	
-	public static void main(String[] args) {
+	public void sendQuery(String query) {
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/qac", "root", "");
 			Statement stmt = conn.createStatement();
-//			stmt.executeUpdate("INSERT INTO school VALUES(2, 'Tom', 40)");
+			stmt.executeUpdate(query);
 			ResultSet rec = stmt.executeQuery("SELECT * FROM school");
 			while(rec.next()) {
-				System.out.println(rec.getInt(1));
-				System.out.println(rec.getString(2));
-				System.out.println(rec.getInt(3));
+				System.out.print(rec.getInt(1) + " ");
+				System.out.print(rec.getString(2) + " ");
+				System.out.print(rec.getInt(3) + "\n");
 			}
 			
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+//		try {
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/qac", "root", "");
+//			Statement stmt = conn.createStatement();
+//			stmt.executeUpdate(query);
+//			ResultSet rec = stmt.executeQuery("SELECT * FROM school");
+//			while(rec.next()) {
+//				System.out.print(rec.getInt(1) + " ");
+//				System.out.print(rec.getString(2) + " ");
+//				System.out.print(rec.getInt(3) + "\n");
+//			}
+//			
+//		}catch(Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
 }
---
+++ JavaDatabase/src/RegisterForm.java
@@ -0,0 +1,103 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class RegisterForm {
+
+	public static void main(String[] args) {
+		
+		ConnectionHandler conn = new ConnectionHandler();
+
+		Frame f = new Frame("Registeration Form");
+		f.addWindowListener(new WindowAdapter() {
+
+			public void windowClosing(WindowEvent e) {
+				super.windowClosing(e);
+				System.exit(0);
+			}
+		});
+
+		Label l1 = new Label("Register Number :");
+		Label l2 = new Label("Name :");
+		Label l3 = new Label("Marks :");
+		Label l4 = new Label("");
+		Label l5 = new Label("");
+		Label l6 = new Label("");
+		TextField t1 = new TextField();
+		TextField t2 = new TextField();
+		TextField t3 = new TextField();
+		
+		Button b1 = new Button("Save");
+		b1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				if(!t1.getText().equals("") && !t2.getText().equals("") && !t3.getText().equals("")) {
+					
+					l4.setText("");
+					l5.setText("");
+					l6.setText("");				
+					
+					try {
+						
+						int regno = Integer.parseInt(t1.getText());
+						String name = t2.getText();
+						int marks = Integer.parseInt(t3.getText());
+						
+						String query = "INSERT INTO school VALUES(" + regno + ", '" + name + "', " + marks + ")";
+						conn.sendQuery(query);
+						
+					}catch(Exception error) {
+						error.printStackTrace();
+					}
+					
+				}
+				else {
+					
+					if(t1.getText().equals("")) {
+						l4.setText("Missing");
+					}
+					else {
+						l4.setText("");
+					}
+					
+					if(t2.getText().equals("")) {
+						l5.setText("Missing");
+					}
+					else {
+						l5.setText("");
+					}
+					
+					if(t3.getText().equals("")) {
+						l6.setText("Missing");
+					}
+					else {
+						l6.setText("");
+					}
+				}
+				
+			}
+		});
+
+		GridLayout g1 = new GridLayout(4, 3);
+		f.setLayout(g1);	
+		f.add(l1);
+		f.add(t1);
+		f.add(l4);
+		f.add(l2);
+		f.add(t2);
+		f.add(l5);
+		f.add(l3);
+		f.add(t3);
+		f.add(l6);
+		f.add(b1);
+
+		f.setSize(500, 200);
+		f.setVisible(true);
+		
+	}
+
+
+
+
+}
